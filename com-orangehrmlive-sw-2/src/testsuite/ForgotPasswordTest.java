a8766682cf0bfb3b0b7a4eeeb2b19c084e443ec1
Jignashathummar <jignashakotadiya10@gmail.com>
2023-11-21 20:36:34 +0000

    update test cases

--- com-orangehrmlive-sw-2/src/testsuite/ForgotPasswordTest.java
+++ com-orangehrmlive-sw-2/src/testsuite/ForgotPasswordTest.java
@@ -1,2 +1,38 @@
-package testsuite;public class ForgotPasswordTest {
+package testsuite;
+/**
+ * 4. Write down the following test into
+ * ‘ForgotPasswordTest’ class
+ * 1. userShouldNavigateToForgotPasswordPageSuccessfull
+ * y
+ * * click on the ‘Forgot your password’ link
+ * * Verify the text ‘Reset Password’
+ */
+
+import browserfactory.BaseTest;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+public class ForgotPasswordTest extends BaseTest {
+    static String baseUrl = "https://opensource-demo.orangehrmlive.com/";
+
+    @Before
+    public void openBrowser() {
+        openBrowser(baseUrl);
+    }
+
+    @Test
+    public void userShouldNavigateToForgotPasswordPageSuccessfully() {
+        //click on the ‘Forgot your password’ link Verify the text ‘Reset Password’
+
+        driver.findElement(By.xpath("//p[text()='Forgot your password? ']")).click();
+        Assert.assertEquals("Reset Password", driver.findElement(By.xpath("//h6[text()='Reset Password']")).getText());
+    }
+
+    @After
+    public void close() {
+        closeBrowser();
+    }
 }
--- com-orangehrmlive-sw-2/src/testsuite/LoginTest.java
+++ com-orangehrmlive-sw-2/src/testsuite/LoginTest.java
@@ -1,2 +1,46 @@
-package testsuite;public class LoginTest {
+package testsuite;
+/**
+ * 2. Create the package ‘testsuite’ and create the
+ * following classes inside the ‘testsuite’ package.
+ * 1. LoginTest
+ * 2. ForgotPasswordTest
+ * 3. Write down the following test into ‘LoginTest’ class
+ * 1. userSholdLoginSuccessfullyWithValidCredentials
+ * * Enter “Admin” username
+ * * Enter “admin123 password
+ * * Click on ‘LOGIN’ button
+ * * Verify the ‘Dashboard’ text is display
+ */
+
+import browserfactory.BaseTest;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+public class LoginTest extends BaseTest {
+    static String baseUrl = "https://opensource-demo.orangehrmlive.com/";
+
+    @Before
+    public void openBrowser() {
+        openBrowser(baseUrl);
+    }
+
+    @Test
+    //Enter “Admin” username
+    //* Enter “admin123 password
+    //* Click on ‘LOGIN’ button
+    //* Verify the ‘Dashboard’ text is display
+    public void userSholdLoginSuccessfullyWithValidCredentials() {
+        driver.findElement(By.name("username")).sendKeys("Admin");
+        driver.findElement(By.name("password")).sendKeys("admin123");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        Assert.assertEquals("Dashboard", driver.findElement(By.xpath("//h6[text()='Dashboard']")).getText());
+    }
+
+    @After
+    public void close() {
+        closeBrowser();
+    }
 }
